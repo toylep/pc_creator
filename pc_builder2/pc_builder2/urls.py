4e2709be3d1fa7ae9f045a00696613855ca43d50
toylep <toylenium@mail.ru>
2024-09-12 16:37:52 -0700

    prettier

--- pc_builder2/pc_builder2/urls.py
+++ pc_builder2/pc_builder2/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     # YOUR PATTERNS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('doc/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path("admin/", admin.site.urls),
     path("api/calculator/", include("calculator.urls")),
