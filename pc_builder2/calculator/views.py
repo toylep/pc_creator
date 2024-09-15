7c636f7db84b0425806c21d03a6946ce6642f6f8
T4 <troltoh@gmail.com>
2024-09-15 18:58:34 +0800

    code review

--- pc_builder2/calculator/views.py
+++ pc_builder2/calculator/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from calculator.tech_city.parser import TechCityParser
 # Create your views here.
+
+class TechCityTestParser(GenericAPIView):
+    
+    def get(self,request):
+        parcer = TechCityParser()
+        #parcer.extraction()
+        return Response({"test": parcer.extraction()})
